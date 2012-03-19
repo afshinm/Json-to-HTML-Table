fcde182f5b9b9db7a45e85c2efe27463a44e8b5a
sgissinger <sgissinger@gmail.com>
2012-03-19 14:19:09 +0100

    * parsedJson parameter should be parsed with any JSON parsing tool : eval(), jQuery.parseJson() or any other one * table headers are retrieved from jsonData itself with the function array_keys() * a table can also be created from a simple string array like ["test", "supertest", "megatest"] * values which looks like a link are formatted as HTML links * values which are null are formatted as NULL

--- json-to-table.js
+++ json-to-table.js
@@ -2,57 +2,98 @@
  * JavaScript format string function
  * 
  */
-
-String.prototype.format = function() {
+String.prototype.format = function()
+{
   var args = arguments;
-  return this.replace(/{(\d+)}/g, function(match, number) {
-    return typeof args[number] != 'undefined'
-      ? args[number]
-      : '{' + number + '}'
-    ;
+
+  return this.replace(/{(\d+)}/g, function(match, number)
+  {
+    return typeof args[number] != 'undefined' ? args[number] :
+                                                '{' + number + '}';
   });
 };
+
 
 /**
  * Convert json to html table
  * 
  * @author Afshin Mehrabani <afshin dot meh at gmail dot com>
  * 
- * @param jsonData string Json data
- * @param keys array Keys for table header
+ * @param parsedJson object Json parsed data
  * @param containerId string Table id 
  * @param tableClassName string Table css class name
  * 
  * @return string Converted json to html table
  */
-function ConvertJsonToTable(jsonData, keys, containerId, tableClassName) {
+function ConvertJsonToTable(parsedJson, containerId, tableClassName)
+{
     //Patterns for table thead & tbody
-    var tbl = "<table border='1' cellpadding='1' cellspacing='1' id='" + containerId + "' class='" + tableClassName + "'>{0}{1}</table>";
-    var th = "<thead>{0}</thead>";
-    var tb = "<tbody>{0}</tbody>";
-    var tr = "<tr>{0}</tr>";
-    var thRow = "<th>{0}</th>";
-    var tdRow = "<td>{0}</td>";
-    var thCon = "";
-    var tbCon = "";
-    var trCon = "";
+    var tbl = '<table border="1" cellpadding="1" cellspacing="1" id="' + containerId + '" class="' + tableClassName + '">{0}{1}</table>';
+    var th = '<thead>{0}</thead>';
+    var tb = '<tbody>{0}</tbody>';
+    var tr = '<tr>{0}</tr>';
+    var link = '<a href="{0}">{0}</a>';
+    var italic = '<i>{0}</i>';
+    var thRow = '<th>{0}</th>';
+    var tdRow = '<td>{0}</td>';
+    var thCon = '';
+    var tbCon = '';
+    var trCon = '';
 
-    if (keys && jsonData) {
+    if (parsedJson)
+    {
+        var isStringArray = typeof(parsedJson[0]) == 'string';
+        var headers;
 
-        //Creating all table headers
-        for (i = 0; i < keys.length; i++) {
-            thCon += thRow.format(eval(keys[i]));
+        // Create table headers from Json data
+        if(isStringArray) // If data is a simple string array we create a single table header
+        {
+            thCon += thRow.format('value');
+        }
+        else
+        {
+            headers = array_keys(parsedJson[0]);    // headers are automatically calcuted 
+
+            for (i = 0; i < headers.length; i++)
+            {
+                thCon += thRow.format(headers[i]);
+            }
         }
         th = th.format(tr.format(thCon));
+        
+        // Create table rows from Json data
+        if(isStringArray)
+        {
+            for (i = 0; i < parsedJson.length; i++)
+            {
+                tbCon += tdRow.format(parsedJson[i]);
+                trCon += tr.format(tbCon);
+                tbCon = '';
+            }
+        }
+        else
+        {
+            var regExp = new RegExp(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig);
 
-        //Creating all table rows from Json data
-        if (typeof(jsonData[0]) == "object") {
-            for (i = 0; i < jsonData.length; i++) {
-                for (j = 0; j < keys.length; j++) {
-                    tbCon += tdRow.format(jsonData[i][keys[j]]);
+            for (i = 0; i < parsedJson.length; i++)
+            {
+                for (j = 0; j < headers.length; j++)
+                {
+                    var value = parsedJson[i][headers[j]];
+                    var isUrl = regExp.test(value);
+
+                    if(isUrl)   // If value is URL we auto-create a link
+                        tbCon += tdRow.format(link.format(value));
+                    else
+                    {
+                        if(value)
+                            tbCon += tdRow.format(value);
+                        else    // If value == null we format it like PhpMyAdmin NULL values
+                            tbCon += tdRow.format(italic.format(value).toUpperCase());
+                    }
                 }
                 trCon += tr.format(tbCon);
-                tbCon = "";
+                tbCon = '';
             }
         }
         tb = tb.format(trCon);
@@ -62,4 +103,45 @@ function ConvertJsonToTable(jsonData, keys, containerId, tableClassName) {
     }
 
     return null;
+}
+
+/**
+ * Return just the keys from the input array, optionally only for the specified search_value
+ * version: 1109.2015
+ *  discuss at: http://phpjs.org/functions/array_keys
+ *  +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+ *  +      input by: Brett Zamir (http://brett-zamir.me)
+ *  +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+ *  +   improved by: jd
+ *  +   improved by: Brett Zamir (http://brett-zamir.me)
+ *  +   input by: P
+ *  +   bugfixed by: Brett Zamir (http://brett-zamir.me)
+ *  *     example 1: array_keys( {firstname: 'Kevin', surname: 'van Zonneveld'} );
+ *  *     returns 1: {0: 'firstname', 1: 'surname'}
+ */
+function array_keys(input, search_value, argStrict)
+{
+    var search = typeof search_value !== 'undefined', tmp_arr = [], strict = !!argStrict, include = true, key = '';
+
+    if (input && typeof input === 'object' && input.change_key_case) { // Duck-type check for our own array()-created PHPJS_Array
+        return input.keys(search_value, argStrict);
+    }
+ 
+    for (key in input)
+    {
+        if (input.hasOwnProperty(key))
+        {
+            include = true;
+            if (search)
+            {
+                if (strict && input[key] !== search_value)
+                    include = false;
+                else if (input[key] != search_value)
+                    include = false;
+            } 
+            if (include)
+                tmp_arr[tmp_arr.length] = key;
+        }
+    }
+    return tmp_arr;
 }
